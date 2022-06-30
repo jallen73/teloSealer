7261fb315b972770a9768b232abcf1543e02dc2d
Sean <biorover@users.noreply.github.com>
2022-06-29 18:32:05 -0700

    trynow

--- scripts/merge_edges.py
+++ scripts/merge_edges.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import argparse
 import sys
+import pandas
 
 def revcomp(x:str) -> str:
     tr = str.maketrans('ATGC','TACG')
@@ -8,7 +9,9 @@ def revcomp(x:str) -> str:
 
 def main():
     parser = argparse.ArgumentParser('merges/patches fastas based on paf alignment')
-    parser.add_argument('--fastas',nargs = '+', help = 'fastas to merge')
+    parser.add_argument('--left', help = 'fasta from left telomere', default = None)
+    parser.add_argument('--right', help = 'fasta from right telomere', default = None)
+    parser.add_argument('--middle', help = 'fasta from main chromosome')
     parser.add_argument('--paf',help = 'paf alignment file')
     parser.add_argument('--max_overhang', 
                         help = 'max bp a hit can overhang the end of the central sequence to be merged completely, default 1000',
@@ -16,45 +19,35 @@ def main():
     args = parser.parse_args()
     
     seqdict = {}
-    for fasta in args.fastas:
-        print('processing fasta file ' + fasta, file = sys.stderr)
-        workingseq = None
-        seq = []
-        with open(fasta) as f:
-            for line in f:
-                if line[0] == ">":
-                    print('processing seq ' + line.split()[0][1:], file = sys.stderr)
-                    if workingseq:
-                        seqdict[workingseq] = "".join(seq)
-                    workingseq = line.split()[0][1:]
-                    seq = []
-                else:
-                    seq.append(line.strip())
-        seqdict[workingseq] = "".join(seq)
+    seqnames = {'left':None,'right':None,'middle':None}
+    for seqn,fasta in zip(['left','middle','right'],[args.left,args.middle,args.right]):
+        if fasta:
+            print('processing fasta file ' + fasta, file = sys.stderr)
+            workingseq = None
+            seq = []
+            with open(fasta) as f:
+                for line in f:
+                    if line[0] == ">":
+                        print('processing seq ' + line.split()[0][1:], file = sys.stderr)
+                        if workingseq:
+                            seqdict[workingseq] = "".join(seq)
+                        workingseq = line.split()[0][1:]
+                        seqnames[seqn] = workingseq
+                        seq = []
+                    else:
+                        seq.append(line.strip())
+            seqdict[workingseq] = "".join(seq)
     outseq = []
     matches = []
-    with open(args.paf) as f:
-        for line in f:
-            fields = line.strip().split('\t')
-            matches.append([fields[5],int(fields[7]),int(fields[8]),int(fields[6]),fields[0],int(fields[2]),int(fields[3]),fields[4]])
-    matches.sort()
-    centralseq = seqdict[matches[0][0]]
-    ltrim = 0
-    for match in matches:
-        if match[1] < args.max_overhang:
-            if match[7] == '+':
-                outseq.append(seqdict[match[4]][:match[6]])
-            elif match[7] == '-':
-                outseq.append(revcomp(seqdict[match[4]][match[5]-1:]))
-            ltrim = match[2]
-        elif match[3] - match[2] < args.max_overhang:
-            outseq.append(centralseq[ltrim:match[1] - 1])
-            outseq.append(seqdict[match[4]][match[5] - 1:])
-        else:
-            outseq.append(centralseq[ltrim:match[1] - 1])
-            ltrim = match[2]
-            outseq.append(seqdict[match[4]][match[5] - 1:match[6]])
-    print('>merged_' + matches[0][0] + '\n' + "".join(outseq))
+    matchdf = pandas.read_csv(args.paf,usecols=list(range(10)))
+    matchdf.columns = ['qid','qlen','qstart','qend','strand','sid','slen','sstart','send','matches','alen']
+    lmatch = matchdf.query(f'qid == "{seqnames['left']}"').sort_values('sstart')
+    rmatch = matchdf.query(f'qid == "{seqnames['right']}"').sort_values('send')
+    lseq = seqdict[seqnames['left']][:list(lmatch['qstart'])[0]]
+    mseq = seqdict[seqnames['middle']][list(lmatch['sstart'])[0]:list(rmatch['send'])[-1]]
+    rseq = seqdict[seqnames['right']][list(rmatch['qend'])[-1]:]
+
+    print('>merged_' + seqnames['middle'] + '\n' + lseq + mseq + rseq)
 
 if __name__ == "__main__":
     main()
